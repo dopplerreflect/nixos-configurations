11af1c544ea5aecea5b305fa0e07b5c9db4d24c0
David Rose <doppler@gmail.com>
2024-07-16 21:37:18 -0500

    remove start-ecowitt.sh

--- hosts/thinkpad/start-ecowitt.sh
+++
@@ -1,3 +0,0 @@
-#! /run/current-system/sw/bin/sh
-/run/current-system/sw/bin/tmux new-session -d -s ecowitt 'cd /home/doppler/code/bun-sveltekit-ecowitt; /run/current-system/sw/bin/bun /home/doppler/Code/bun-sveltekit-ecowitt/build/index.js'
-
