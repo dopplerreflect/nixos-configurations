1accae1ad176a510d22494b862a9d74c790059f7
David Rose <doppler@gmail.com>
2024-07-17 02:36:51 +0000

    create_ap use channel 3

--- hosts/pi/default.nix
+++ hosts/pi/default.nix
@@ -53,6 +53,7 @@
   services.create_ap = {
     enable = true;
     settings = {
+      CHANNEL=3;
       ETC_HOSTS=1;
       DHCP_HOSTS = "thinkpad GW2000X";
       DHCP_DNS = "192.168.12.1,8.8.8.8,8.8.4.4,1.1.1.1";
