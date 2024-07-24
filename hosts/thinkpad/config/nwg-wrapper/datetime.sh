5de346233f21b33baab5f93a1a86dea756497084
David Rose <doppler@gmail.com>
2024-07-24 17:55:52 -0500

    remove nwg config dirs

--- hosts/thinkpad/config/nwg-wrapper/datetime.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-date=$(date +'%A, %B %d')
-time=$(date +'%H:%M:%S')
-
-echo '<span size="24000" foreground="#FFBF00">'$date' '$time'</span>'
-#echo '<span size="24000" foreground="#000000">'$date' '$time'</span>'
-
