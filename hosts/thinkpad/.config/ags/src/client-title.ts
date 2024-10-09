d1c58698240c9b2e59f7f93d2bb7a70b9e2e49fc
David Rose <doppler@gmail.com>
2024-10-08 19:19:34 -0500

    ags: trim client-title to 110 chars

--- hosts/thinkpad/.config/ags/src/client-title.ts
+++ hosts/thinkpad/.config/ags/src/client-title.ts
@@ -2,7 +2,9 @@ import { hyprland } from "./services";
 const ClientTitle = () =>
   Widget.Label({
     class_name: "client-title",
-    label: hyprland.active.client.bind("title"),
+    label: hyprland.active.client
+      .bind("title")
+      .as(title => `${title}`.slice(0, 110)),
   });
 
 export default ClientTitle;
