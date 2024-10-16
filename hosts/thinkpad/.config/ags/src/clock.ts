811c9708c46249881d2d06c0363b7648bdacec6c
David Rose <doppler@gmail.com>
2024-10-15 21:04:43 -0500

    new ags bar

--- hosts/thinkpad/.config/ags/src/clock.ts
+++ hosts/thinkpad/.config/ags/src/clock.ts
@@ -2,11 +2,6 @@ const time = Variable("", {
   poll: [1000, 'date "+%a %b %d %H:%M:%S"'],
 });
 
-// const Clock = () =>
-//   Widget.Label({
-//     class_name: "clock",
-//     label: time.bind(),
-//   });
 const Clock = () =>
   Widget.Button({
     child: Widget.Label({
@@ -26,19 +21,5 @@ export const ClockWindow = Widget.Window({
     label: time.bind(),
   }),
 }).keybind("Escape", () => App.closeWindow("clock-window"));
-
-export const SmallClockWindow = Widget.Window({
-  name: "small-clock-window",
-  layer: "overlay",
-  visible: true,
-  anchor: ["bottom", "right"],
-  child: Widget.Button({
-    child: Widget.Label({
-      class_name: "clock",
-      label: time.bind(),
-    }),
-    onClicked: () => App.toggleWindow("clock-window"),
-  }),
-});
 
 export default Clock;
