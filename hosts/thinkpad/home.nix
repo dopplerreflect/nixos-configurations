69391e477eba9b3adac6f707407bd468aa0facd7
David Rose <doppler@gmail.com>
2024-10-31 11:43:43 -0500

    thinkpad: remove unused apps

--- hosts/thinkpad/home.nix
+++ hosts/thinkpad/home.nix
@@ -6,53 +6,21 @@
     homeDirectory = "/home/doppler";
     packages = with pkgs; [
       # ags # this is in nix profile instead due to broken upstream build
-      bc
       bitwarden
       brave
       btop
-      dconf-editor
-      # easyeffects # php requirement was failing test on nixos-unstable 7/31/2024
-      ffmpeg
-      ffmpegthumbnailer
-      file
-      firefox
-      freefilesync
-      fzf
-      gimp
-      gnome-characters
-      gnome-tweaks
-      googleearth-pro
       # gqrx
-      gucharmap
-      heroku
-      imagemagick
       imv
-      inkscape
-      jq
       kitty
-      miller # miller text parser thing https://miller.readthedocs.io/en/latest/
-      morewaita-icon-theme
       mpv
       nautilus
       nextcloud-client
-      nmap
       nodejs
-      pavucontrol
       pnpm
-      pulseaudioFull
-      python3
-      pywal
-      ripgrep
-      rpi-imager
-      rtl-sdr
-      rtl_433
-      sqlite-interactive
-      unzip
       virt-manager
       virt-viewer
       vscodium
       wavemon
-      wget
       xfce.thunar
       xfce.thunar-volman
       xfce.tumbler
