2771accf567672a65cbff79cf5df768478f7fa9b
David Rose <doppler@gmail.com>
2024-11-01 08:43:58 -0500

    mv hyprscale to hyprland config and add bc dependency

--- hosts/thinkpad/hyprland/default.nix
+++ hosts/thinkpad/hyprland/default.nix
@@ -8,18 +8,24 @@
     };
   };
 
-  home.packages = with pkgs; [
-    brightnessctl
-    hypridle
-    hyprlock
-    hyprshot
-    libnotify
-    swaynotificationcenter
-    swww
-    wl-clipboard
-    wlroots
-    xdg-desktop-portal-hyprland
-  ];
+  home = {
+    packages = with pkgs; [
+      bc
+      brightnessctl
+      hypridle
+      hyprlock
+      hyprshot
+      libnotify
+      swaynotificationcenter
+      swww
+      wl-clipboard
+      wlroots
+      xdg-desktop-portal-hyprland
+    ];
+    file = {
+      "./.local/bin/hyprscale".source = ./hyprscale;
+    };
+  };
 
   xdg.configFile = {
     "hypr/hypridle.conf".source = ./hypridle.conf;
