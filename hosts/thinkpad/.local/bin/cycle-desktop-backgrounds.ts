29785f744486a9d1d6ba6eea91efabea42aadd7a
David Rose <doppler@gmail.com>
2024-10-31 21:51:30 -0500

    add tsconfig for bun scripts

--- hosts/thinkpad/.local/bin/cycle-desktop-backgrounds.ts
+++ hosts/thinkpad/.local/bin/cycle-desktop-backgrounds.ts
@@ -3,21 +3,26 @@
 import { readdir } from "node:fs/promises";
 import { $ } from "bun";
 
+type Monitor = {
+  name: string;
+};
+
 const DIR = "/home/doppler/Pictures/Desktop-Backgrounds";
 
 const files = await readdir(DIR);
 const paths = files.map(file => `${DIR}/${file}`);
 
-const getRandomPath = () => {
+const getRandomPath = (): string => {
   let path = paths[Math.floor(Math.random() * paths.length) + 1];
   return path || getRandomPath();
 };
 
-const getMonitors = async () => {
+const getMonitors = async (): Promise<Monitor[]> => {
   let shellResult = await $`hyprctl monitors -j`.quiet();
   return shellResult.json();
 };
-const swapImages = async () => {
+
+const swapImages = async (): Promise<void> => {
   try {
     let monitors = await getMonitors();
     monitors.forEach(async monitor => {
