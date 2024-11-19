817dc4658c44c2b0190d7566ac552e76d73f8c69
David Rose <doppler@gmail.com>
2024-11-19 09:24:44 -0600

    add change-desktop-backgrounds.ts

---
+++ hosts/thinkpad/.local/bin/change-desktop-backgrounds.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env bun
+
+import { readdir } from "node:fs/promises";
+import { $ } from "bun";
+
+type Monitor = {
+  name: string;
+};
+
+const DIR = "/home/doppler/Pictures/Desktop-Backgrounds";
+
+const files = await readdir(DIR);
+const paths = files.map(file => `${DIR}/${file}`);
+
+const getRandomPath = (): string => {
+  let path = paths[Math.floor(Math.random() * paths.length) + 1];
+  return path || getRandomPath();
+};
+
+const getMonitors = async (): Promise<Monitor[]> => {
+  let shellResult = await $`hyprctl monitors -j`.quiet();
+  return shellResult.json();
+};
+
+const swapImages = async (): Promise<void> => {
+  try {
+    let monitors = await getMonitors();
+    monitors.forEach(async monitor => {
+      let path = getRandomPath();
+      console.info(monitor.name, path);
+      await $`swww img -o ${monitor.name} ${path}`;
+    });
+  } catch (e) {}
+};
+
+// setInterval(swapImages, 1000 * 60 * 5);
+swapImages();
