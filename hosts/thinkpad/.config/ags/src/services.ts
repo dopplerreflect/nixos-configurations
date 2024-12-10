0c46280920cb4d43789ca4ac961b2cdc8eaf6fee
David Rose <doppler@gmail.com>
2024-12-10 17:26:47 -0600

    remove ags config

--- hosts/thinkpad/.config/ags/src/services.ts
+++
@@ -1,5 +0,0 @@
-const audio = await Service.import("audio");
-const hyprland = await Service.import("hyprland");
-const network = await Service.import("network");
-
-export { audio, hyprland, network };
