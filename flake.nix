f688b5254dcad49f40f82fe6c622e7247a708000
David Rose <doppler@gmail.com>
2025-01-19 22:30:29 -0600

    update lix
    Version changes:
    [C.]  #1  gcc   14-20241116-lib x3, 14-20241116-libgcc x3 -> 14-20241116, 14-20241116-lib x3, 14-20241116-libgcc x3
    [U*]  #2  lix   2.91.1, 2.91.1-doc -> 2.92.0, 2.92.0-doc
    [C.]  #3  mpfr  4.2.1 -> 4.2.1 x2
    Added packages:
    [A.]  #1  capnproto      1.0.2
    [A.]  #2  gmp            6.3.0
    [A.]  #3  isl            0.20
    [A.]  #4  libmpc         1.3.1
    [A.]  #5  nlohmann_json  3.11.3
    [A.]  #6  toml11         3.7.1
    Closure size: 1994 -> 2002 (47 paths added, 39 paths removed, delta +8, disk usage +276.5MiB).

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # For getting the cachyos kernel
