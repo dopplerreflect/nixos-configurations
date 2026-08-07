8416acaa59d4878e3bd35700579ab9b826bde750
David Rose <doppler@gmail.com>
2026-08-07 09:58:35 -0500

    programs/hyprland: disable systemd

--- programs/hyprland/default.nix
+++ programs/hyprland/default.nix
@@ -7,7 +7,7 @@
       enable = true;
       configType = "lua";
       extraConfig = builtins.readFile ./hyprland-with-dms.lua;
-      systemd.enable = true;
+      systemd.enable = false;
     };
   };
 }
